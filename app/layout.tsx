39e4a9739831870fc09f58d07d3eba0cdf782958
alouzizi <alouzizi@student.1337.ma>
2024-08-29 11:26:35 +0100

    feat: :white_check_mark: Update metadata and add favicon for portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,31 @@ import { ThemeProvider } from "@/components/theme-provider"
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ali Louzizi",
-  description: "Ali Louzizi's personal website",
+  metadataBase: new URL("https://www.alilouzizi.me/"),
+
+  title: {
+    template: "%s | Ali Louzizi",
+    default: "Ali Louzizi",
+  },
+  authors: {
+    name: "Ali Louzizi",
+  },
+
+  description:
+    "Explore the portfolio of Ali Louzizi, a Full Stack Developer specializing in modern web applications. Discover projects that showcase expertise in Next.js, Supabase, and more.",
+  openGraph: {
+    title: "Ali Louzizi",
+    description:
+      "Explore the portfolio of Ali Louzizi, a Full Stack Developer specializing in modern web applications. Discover projects that showcase expertise in Next.js, Supabase, and more.",
+    url: "https://www.alilouzizi.me/",
+    siteName: "Ali Louzizi",
+    images: "/og.png",
+    type: "website",
+  },
+  keywords: ["Ali Louzizi", "Full Stack Developer", "Next.js", "Supabase", "Web Development"],
 };
+
+
 
 export default function RootLayout({
   children,
